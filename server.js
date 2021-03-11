1e103a85c507ef3a3f81dbde7247ca9c1d176181
Jenn Greiner <jenn.greiner1@gmail.com>
2021-03-10 19:46:37 -0700

    routes

--- server.js
+++ server.js
@@ -12,11 +12,21 @@ app.listen(PORT, function () {
     console.log(`Listening at port ${PORT}`)
 });
 
+const tables = [
+    {
+        routeName: 'dan',
+        name: 'dan',
+        phoneNumber: '8888888888',
+        email: 'dan@dan.com',
+        id: 'dan',
+    },
+]
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Route
-app.get("/", function (req, res) {
-    res.json(path.join(__dirname, "public/home.html"));
-});
+app.get("/", (req, res) => res.send(path.join(__dirname, "home.html")));
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+app.get('/reservation', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
