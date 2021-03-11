e560725ad15940ea1f701584bbb97d2131a2d03d
Jenn Greiner <jenn.greiner1@gmail.com>
2021-03-10 20:34:35 -0700

    start of res JS

--- server.js
+++ server.js
@@ -33,3 +33,19 @@ app.use(express.json());
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 app.get('/reservation', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+app.get('/api/tables', (req, res) => res.json(tables));
+
+// Create New Reservation - takes in JSON input
+app.post('/api/tables', (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    const newTable = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newTable.routeName = newTable.name.replace(/\s+/g, '').toLowerCase();
+    console.log(newTable);
+
+    tables.push(newTable);
+    res.json(newTable);
+});
