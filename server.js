5849fadd394f25418cf9052c10af91085577a4fe
Jenn Greiner <jenn.greiner1@gmail.com>
2021-03-10 19:04:23 -0700

    front-end visuals

---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Route
+app.get('/', (req, res) => console.log(`Listening at port ${PORT}`));
